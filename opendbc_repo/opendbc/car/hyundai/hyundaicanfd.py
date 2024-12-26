c9b5b7e8350f1e0628154fafb9a7daa98b781777
multikyd <multikyd@gmail.com>
2024-12-26 22:51:25 +0900

    test

--- opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
@@ -68,10 +68,9 @@ def create_steering_messages(packer, CP, CAN, enabled, lat_active, steering_pres
       "TORQUE_REQUEST": apply_steer,
       "LKA_ASSIST": 0,
       "STEER_REQ": 1 if lat_active else 0,
-      #"STEER_MODE": 31,
+      "STEER_ACT": 131 if lat_active else 100,
       "HAS_LANE_SAFETY": 0,  # hide LKAS settings
-      "NEW_SIGNAL_1": 0,
-      "NEW_SIGNAL_2": 3,
+      "NEW_SIGNAL_1": 3 if lat_active else 0,
       "NEW_SIGNAL_3": 0,
       "NEW_SIGNAL_4": 0,
     }
